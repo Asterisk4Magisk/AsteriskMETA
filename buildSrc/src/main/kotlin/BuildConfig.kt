0d0682c0697b050e1f907077d2feb579c04afc75
Whale Choi <cj1369636717@gmail.com>
2026-07-20 12:04:01 +0800

    bump version 1.1.2

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -11,7 +11,7 @@ import org.gradle.api.tasks.TaskAction
 object ProjectConfig {
     const val JVM_VERSION = 26
     const val PROJECT_NAME = "AsteriskMETA"
-    const val VERSION_NAME = "1.1.2-dev"
+    const val VERSION_NAME = "1.1.2"
     const val PACKAGE_NAME = "org.asterisk.zcc.ameta"
     const val ASTERISKD_VERSION = "v1.0.0"
     const val BPF2SOCKS_VERSION = "v1.0.0"
