2836a604ce74f2d724a22a411ec533534cc7c6ba
Whale Choi <cj1369636717@gmail.com>
2026-08-16 01:18:36 +0800

    chore: bump dependencies version

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -18,7 +18,7 @@ object ProjectConfig {
     const val BPF_MATCHER_VERSION = "v1.0.1"
     const val CMFA_WRAPPER_VERSION = "v2.11.32-asterisk"
     const val MIHOMO_CORE_VERSION = "v1.19.29"
-    const val HEV_SOCKS5_TUNNEL_VERSION = "2.17.0"
+    const val HEV_SOCKS5_TUNNEL_VERSION = "2.17.1"
     const val TARGET_SDK = 37
     const val MIN_SDK = 24
     val SUPPORTED_ANDROID_ABIS = listOf("arm64-v8a", "armeabi-v7a", "x86", "x86_64")
