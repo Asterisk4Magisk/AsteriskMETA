5f425d401ca887317707a01a30eb308a62050939
Whale Choi <cj1369636717@gmail.com>
2026-08-18 18:44:47 +0800

    refactor: replace recovery paths with stateless reconciliation

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -11,9 +11,9 @@ import org.gradle.api.tasks.TaskAction
 object ProjectConfig {
     const val JVM_VERSION = 26
     const val PROJECT_NAME = "AsteriskMETA"
-    const val VERSION_NAME = "1.2.1"
+    const val VERSION_NAME = "1.2.2-dev"
     const val PACKAGE_NAME = "org.asterisk.zcc.ameta"
-    const val ASTERISKD_VERSION = "v2.0.12"
+    const val ASTERISKD_VERSION = "v2.0.13"
     const val BPF2SOCKS_VERSION = "v1.0.5"
     const val BPF_MATCHER_VERSION = "v1.0.1"
     const val CMFA_WRAPPER_VERSION = "v2.11.33-asterisk"
