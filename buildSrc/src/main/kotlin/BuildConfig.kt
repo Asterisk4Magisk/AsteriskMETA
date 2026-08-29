fca9a6283929587d23b387c23a1576ab611fb22b
Whale Choi <cj1369636717@gmail.com>
2026-08-29 20:33:24 +0800

    fix: exempt ipsec interfaces from IPv6 guard

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -13,7 +13,7 @@ object ProjectConfig {
     const val PROJECT_NAME = "AsteriskMETA"
     const val VERSION_NAME = "1.2.5-dev"
     const val PACKAGE_NAME = "org.asterisk.zcc.ameta"
-    const val ASTERISKD_VERSION = "v2.0.22"
+    const val ASTERISKD_VERSION = "v2.0.23"
     const val BPF2SOCKS_VERSION = "v1.0.10"
     const val BPF_MATCHER_VERSION = "v1.0.1"
     const val CMFA_WRAPPER_VERSION = "v2.11.33-asterisk"
